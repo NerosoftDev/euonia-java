82ec8d96d72b4cd3340712f2587d1694646d801d
damon <zhaorong@outlook.com>
2026-06-06 14:47:15 +0800

    Add RuleCheckException class to handle validation errors during rule checks

---
+++ osba/src/main/java/com/euonia/osba/rules/RuleCheckException.java
@@ -0,0 +1,21 @@
+package com.euonia.osba.rules;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RuleCheckException extends RuntimeException {
+
+    private final Map<String, List<String>> errors = new HashMap<>();
+
+    private final static String MESSAGE = "Object not valid for save.";
+
+    public RuleCheckException(Map<String, List<String>> errors) {
+        super(MESSAGE);
+        this.errors.putAll(errors);
+    }
+
+    public Map<String, List<String>> getErrors() {
+        return Map.copyOf(errors);
+    }
+}
