704728d07ad8804dd10d950bc159be267efe1c7f
damon <zhaorong@outlook.com>
2026-06-12 17:21:11 +0800

    feat: Add CallOptions, ExtendableOptions, PublishOptions, and SendOptions classes for enhanced message handling

---
+++ bus-core/src/main/java/com/euonia/bus/options/CallOptions.java
@@ -0,0 +1,13 @@
+package com.euonia.bus.options;
+
+public class CallOptions extends ExtendableOptions {
+    private String correlationId;
+
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+}
